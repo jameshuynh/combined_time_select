b17cea7d4ea8f6b712dbeae5e368daf526c243f3
James Huynh <james@rubify.com>
2017-07-20 23:31:27 +0800

    change all alias_method_chain

--- lib/combined_time_select.rb
+++ lib/combined_time_select.rb
@@ -4,7 +4,6 @@ module CombinedTimeSelect
   # Your code goes here...
 end
 
-
 module ActiveSupport
   class HashWithIndifferentAccess < Hash
     def parse_time_select!(attribute)
@@ -18,97 +17,107 @@ end
 
 module ActionView::Helpers
   class DateTimeSelector
-     def select_minute_with_simple_time_select
-        return select_minute_without_simple_time_select unless @options[:combined].eql? true
+    def select_minute_with_simple_time_select
+      return select_minute_without_simple_time_select unless @options[:combined].eql? true
 
-        # Although this is a datetime select, we only care about the time.  Assume that the date will
-        # be set by some other control, and the date represented here will be overriden
+      # Although this is a datetime select, we only care about the time.  Assume that the date will
+      # be set by some other control, and the date represented here will be overriden
 
-        val_minutes = @datetime.kind_of?(Time) ? @datetime.min + @datetime.hour*60 : @datetime
+      val_minutes = @datetime.kind_of?(Time) ? @datetime.min + @datetime.hour*60 : @datetime
 
-        @options[:time_separator] = ""
+      @options[:time_separator] = ""
 
-        # Default is 15 minute intervals
-        minute_interval = @options.fetch(:minute_interval, 15)
+      # Default is 15 minute intervals
+      minute_interval = @options.fetch(:minute_interval, 15)
 
-        start_minute = 0
-        end_minute   = 1439
+      start_minute = 0
+      end_minute   = 1439
 
-        # @options[:start_hour] should be specified in military
-        # i.e. 0-23
-        if @options[:start_hour]
-          start_minute =  @options[:start_hour] * 60
-          start_minute += @options.fetch(:start_minute, 0)
-        end
+      # @options[:start_hour] should be specified in military
+      # i.e. 0-23
+      if @options[:start_hour]
+        start_minute =  @options[:start_hour] * 60
+        start_minute += @options.fetch(:start_minute, 0)
+      end
 
-        # @options[:end_hour] should be specified in military
-        # i.e. 0-23
-        if @options[:end_hour]
-          end_minute = (@options[:end_hour] * 60) + @options.fetch(:end_minute, 59)
-        end
+      # @options[:end_hour] should be specified in military
+      # i.e. 0-23
+      if @options[:end_hour]
+        end_minute = (@options[:end_hour] * 60) + @options.fetch(:end_minute, 59)
+      end
 
-        if @options[:use_hidden] || @options[:discard_minute]
-          build_hidden(:minute, val)
-        else
-          minute_options = []
-          start_minute.upto(end_minute) do |minute|
-            if minute%minute_interval == 0
-              ampm = minute < 720 ? ' AM' : ' PM'
-              hour = minute/60
-              minute_padded = zero_pad_num(minute%60)
-              hour_padded = zero_pad_num(hour)
-              ampm_hour = ampm_hour(hour)
+      if @options[:use_hidden] || @options[:discard_minute]
+        build_hidden(:minute, val)
+      else
+        minute_options = []
+        start_minute.upto(end_minute) do |minute|
+          if minute%minute_interval == 0
+            ampm = minute < 720 ? ' AM' : ' PM'
+            hour = minute/60
+            minute_padded = zero_pad_num(minute%60)
+            hour_padded = zero_pad_num(hour)
+            ampm_hour = ampm_hour(hour)
 
-              val = "#{hour_padded}:#{minute_padded}:00"
+            val = "#{hour_padded}:#{minute_padded}:00"
 
-              option_text = @options[:ampm] ? "#{ampm_hour}:#{minute_padded}#{ampm}" : "#{hour_padded}:#{minute_padded}"
-              minute_options << ((val_minutes == minute) ?
-                %(<option value="#{val}" selected="selected">#{option_text}</option>\n) :
-                %(<option value="#{val}">#{option_text}</option>\n)
+            option_text = @options[:ampm] ? "#{ampm_hour}:#{minute_padded}#{ampm}" : "#{hour_padded}:#{minute_padded}"
+            minute_options << ((val_minutes == minute) ?
+                               %(<option value="#{val}" selected="selected">#{option_text}</option>\n) :
+            %(<option value="#{val}">#{option_text}</option>\n)
               )
-            end
           end
-          build_select(:minute, minute_options.join(' '))
         end
+        build_select(:minute, minute_options.join(' '))
       end
-      alias_method_chain :select_minute, :simple_time_select
+    end
 
+    alias select_minute_without_simple_time_select select_minute
+    alias select_minute select_minute_with_simple_time_select
 
-      def select_hour_with_simple_time_select
-        return select_hour_without_simple_time_select unless @options[:combined].eql? true
-        # Don't build the hour select
-        #build_hidden(:hour, val)
-      end
-      alias_method_chain :select_hour, :simple_time_select
+    def select_hour_with_simple_time_select
+      return select_hour_without_simple_time_select unless @options[:combined].eql? true
+      # Don't build the hour select
+      # build_hidden(:hour, val)
+    end
 
-      def select_second_with_simple_time_select
-        return select_second_without_simple_time_select unless @options[:combined].eql? true
-        # Don't build the seconds select
-        #build_hidden(:second, val)
-      end
-      alias_method_chain :select_second, :simple_time_select
+    alias select_hour_without_simple_time_select select_hour
+    alias select_hour select_hour_with_simple_time_select
 
-      def select_year_with_simple_time_select
-        return select_year_without_simple_time_select unless @options[:combined].eql? true
-        # Don't build the year select
-        #build_hidden(:year, val)
-      end
-      alias_method_chain :select_year, :simple_time_select
+    def select_second_with_simple_time_select
+      return select_second_without_simple_time_select unless @options[:combined].eql? true
+      # Don't build the seconds select
+      # build_hidden(:second, val)
+    end
 
-      def select_month_with_simple_time_select
-        return select_month_without_simple_time_select unless @options[:combined].eql? true
-        # Don't build the month select
-        #build_hidden(:month, val)
-      end
-      alias_method_chain :select_month, :simple_time_select
+    alias select_second_without_simple_time_select select_second
+    alias select_second select_second_with_simple_time_select
 
-      def select_day_with_simple_time_select
-        return select_day_without_simple_time_select unless @options[:combined].eql? true
-        # Don't build the day select
-        #build_hidden(:day, val)
-      end
-      alias_method_chain :select_day, :simple_time_select
+    def select_year_with_simple_time_select
+      return select_year_without_simple_time_select unless @options[:combined].eql? true
+      # Don't build the year select
+      # build_hidden(:year, val)
+    end
 
+    alias select_year_without_simple_time_select select_year
+    alias select_year select_year_with_simple_time_select
+
+    def select_month_with_simple_time_select
+      return select_month_without_simple_time_select unless @options[:combined].eql? true
+      # Don't build the month select
+      # build_hidden(:month, val)
+    end
+
+    alias select_month_without_simple_time_select select_month
+    alias select_month select_month_with_simple_time_select
+
+    def select_day_with_simple_time_select
+      return select_day_without_simple_time_select unless @options[:combined].eql? true
+      # Don't build the day select
+      # build_hidden(:day, val)
+    end
+
+    alias select_day_without_simple_time_select select_day
+    alias select_day select_day_with_simple_time_select
   end
 end
 
